1c3b6c0f7e1f07582083edd24612de434492e2d8
Louiseykat <louisekriek90@gmail.com>
2024-02-21 18:44:40 +0200

    updated challenge after coaching session

--- challenge1.js
+++ challenge1.js
@@ -1,13 +1,15 @@
 let date = 2050;
 let position = "parent";
 let count = 0;
+let month;
 
 console.log(count);
 
 if (date === 2050) {
 	console.log("January", "New Year’s Day");
 	console.log("March", "Human Rights Day");
-	console.log("April", "Family Day", "Freedom Day");
+	month = "April";
+	console.log(month, "Family Day", "Freedom Day");
 	count += 4;
 
 	if (position === "student") {
@@ -17,18 +19,19 @@ if (date === 2050) {
 
 	console.log("August", "Women's Day");
 	console.log("September", "Heritage Day");
-	console.log("December", "Day of Reconciliation");
+	month = "December";
+	console.log(month, "Day of Reconciliation");
 	count += 3;
 
 	if (position === "parent") {
-	  	console.log("December", "Christmas Day");
-		count +=+ 1;
+	  	console.log(date, "Christmas Day");
+		count += 1;
   }
 
 	console.log("December", "Day of Goodwill");
-	count +=+ 1;
+	count += 1;
 }
 
-console.log('Your status is:', position)
-console.log('The year is:', date)
-console.log('The total holidays is:', count)
+console.log('Your status is:', position);
+console.log('The year is:', date);
+console.log('The total holidays is:', count);
